79e45e2dfd727881ed014e6243ea2862708821df
David Cramer <dcramer@gmail.com>
2013-01-11 12:34:14 -0800

    0.2.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ install_requires = [
 
 setup(
     name='sentry-webhooks',
-    version='0.1.0',
+    version='0.2.0',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/getsentry/sentry-webhooks',
